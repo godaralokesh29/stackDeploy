26a6f9ef55d7a22f7e3eb5b6f6ea64a0484ba717
godaralokesh29 <godaralokesh2023@gmail.com>
2026-06-18 01:05:04 +0530

    added the generate function

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+const MAX_LEN=5;
+
+ function generate() {
+    let ans="";
+    let string ="123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i=0;i<MAX_LEN;i++){
+        ans+=string.charAt(Math.floor(Math.random() * string.length));
+    }
+    return ans;
+
+}
