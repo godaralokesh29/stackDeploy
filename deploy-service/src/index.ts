8d3e8a32e775e599e6506b3d1853a1a80c965542
godaralokesh29 <godaralokesh2023@gmail.com>
2026-06-24 23:15:36 +0530

    feat: deploy services initiated

---
+++ deploy-service/src/index.ts
@@ -0,0 +1,17 @@
+import {createClient,commandOptions } from "redis";
+const subscriber=createClient();
+subscriber.connect();
+
+async function main() {
+    while(1){
+        const response= await subscriber.brPop(
+            commandOptions({isolated:true}),
+            'build-queue',
+            0
+
+
+        );
+
+    }
+    
+}
